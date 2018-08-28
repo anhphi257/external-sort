ee54a5548b8716934f8ff28bb5d355c97304a860
Hoang Anh Phi <phiha@cyberspace.vn>
2018-08-28 15:23:32 +0700

    it works

--- src/main/java/com/github/anhphi257/sort/ExternalSort.java
+++ src/main/java/com/github/anhphi257/sort/ExternalSort.java
@@ -7,11 +7,13 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 public class ExternalSort {
+    private static String TMP_DIR = "tmp";
     private static Charset DEFAULT_CHARSET = StandardCharsets.UTF_8;
     private static Comparator<String> DEFAULT_COMPARATOR = Comparator.naturalOrder();
     private int numThreads;
@@ -27,9 +29,24 @@ public class ExternalSort {
     public ExternalSort(int numThreads) {
         this.numThreads = numThreads;
         this.blockSize = estimasteBlockSize();
-        this.queue = new ArrayBlockingQueue<>(4);
+        this.queue = new ArrayBlockingQueue<>(numThreads);
         System.out.println("Number threads: " + this.numThreads);
         System.out.println("Block size: " + this.blockSize);
+    }
+
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException, IOException {
+        if (args.length < 2) {
+            System.out.println("Usage: java -Xmx<MEM_SIZE> -cp external-sort-1.0-SNAPSHOT.jar com.github.anhphi257.sort.ExternalSort" +
+                    " <input> <output> <numThreads>");
+            System.exit(0);
+        }
+        String input = args[0];
+        String output = args[1];
+
+        int numThread = Integer.parseInt(args[2]);
+        ExternalSort sort = new ExternalSort(numThread);
+        sort.sort(input, output);
     }
 
     private long estimasteBlockSize() {
@@ -38,7 +55,7 @@ public class ExternalSort {
         long usedMem = r.totalMemory() - r.freeMemory();
         long freeMem = r.maxMemory() - usedMem;
         System.out.println("Free mem: " + freeMem);
-        return freeMem / (numThreads * 16 + 1);
+        return freeMem / (numThreads * 16);
     }
 
     public void sort(String input, String output) throws IOException, ExecutionException, InterruptedException {
@@ -46,8 +63,14 @@ public class ExternalSort {
     }
 
     public void sort(File input, File output) throws IOException, ExecutionException, InterruptedException {
+        long start = System.currentTimeMillis();
         List<File> files = splitAndSort(input);
+        long now = System.currentTimeMillis();
+        System.out.printf("split and sort took %dms\n", now - start);
         mergeSortedFiles(files, output);
+        long end = System.currentTimeMillis();
+        System.out.printf("merging took %dms\n", end - now);
+        System.exit(0);
     }
 
     //2-passes merging
@@ -63,7 +86,7 @@ public class ExternalSort {
         int fromIndex = 0;
         while (fromIndex < files.size()) {
             int toIndex = Math.min(fromIndex + batch, files.size());
-            File tmp = Paths.get("..", "tmp2", fromIndex + "-" + toIndex).toFile(); //TODO: implement output path
+            File tmp = Paths.get(TMP_DIR, "merge" + fromIndex + "-" + toIndex).toFile(); 
             Future<?> future = executor.submit(new Merger(files.subList(fromIndex, toIndex), tmp));
             firstPassMergeFiles.add(tmp);
             firstPassMergeFutures.add(future);
@@ -81,7 +104,6 @@ public class ExternalSort {
 
 
     private List<File> splitAndSort(File input) throws IOException, InterruptedException, ExecutionException {
-        long start = System.currentTimeMillis();
         AtomicInteger numFile = new AtomicInteger();
         String tmpDir = Helper.createTemperaryDirectory();
         List<File> files = new ArrayList<>();
@@ -100,7 +122,7 @@ public class ExternalSort {
                     } else {
                         Runtime runtime = Runtime.getRuntime();
                         System.out.println("Add to queue " + lines.size() + " lines");
-                        System.out.println(runtime.totalMemory() - runtime.freeMemory());
+                        System.out.println("Current used memory in heap: " + (runtime.totalMemory() - runtime.freeMemory()) + " bytes");
                         queue.put(lines);
                         numFile.incrementAndGet();
                         lines = new ArrayList<>();
@@ -143,7 +165,7 @@ public class ExternalSort {
                             numFile.decrementAndGet();
                             System.gc();
                             files.add(file);
-                            System.out.println("Write done. Current queue size: " + queue.size());
+//                            System.out.println("Write done. Current queue size: " + queue.size());
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
@@ -158,16 +180,14 @@ public class ExternalSort {
         readDone = Helper.waitExecution(readerFuture);
         Helper.waitExecution(sorterFutures);
 
-        long end = System.currentTimeMillis();
-        System.out.println("Split and sort in: " + (end - start) + " ms");
         return files;
     }
 
 
     private static class Helper {
 
-        public static String createTemperaryDirectory() {
-            String dirPath = Paths.get("E:\\code", "tmp").toString();
+        static String createTemperaryDirectory() {
+            String dirPath = Paths.get(TMP_DIR).toString();
             File file = new File(dirPath);
             if (file.exists()) {
                 if (file.isDirectory()) {
@@ -182,13 +202,13 @@ public class ExternalSort {
             return dirPath;
         }
 
-        public static void waitExecution(Future... futures) {
+        static void waitExecution(Future... futures) {
             for (Future f : futures) {
                 waitExecution(f);
             }
         }
 
-        public static <T> T waitExecution(Future<T> future) {
+        static <T> T waitExecution(Future<T> future) {
             try {
                 return future.get();
             } catch (InterruptedException | ExecutionException e) {
@@ -197,8 +217,73 @@ public class ExternalSort {
             return null;
         }
 
-        public static void waitExecution(List<Future> futures) {
+        static void waitExecution(List<Future> futures) {
             waitExecution(futures.toArray(new Future[futures.size()]));
         }
     }
+
+    final class Merger implements Runnable {
+        private List<File> files;
+        private File outFile;
+
+        public Merger(List<File> files, File out) {
+            this.files = files;
+            this.outFile = out;
+        }
+
+
+        @Override
+        public void run() {
+            try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(outFile), DEFAULT_CHARSET))) {
+                PriorityQueue<FileCache> queue = new PriorityQueue<>(Comparator.comparing(FileCache::peek));
+                for (File file : files) {
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), DEFAULT_CHARSET));
+                    FileCache fileCache = new FileCache(reader);
+                    if (!fileCache.empty())
+                        queue.add(fileCache);
+                }
+
+                while (!queue.isEmpty()) {
+                    FileCache cache = queue.poll();
+                    writer.write(cache.poll());
+                    writer.newLine();
+                    if (!cache.empty()) {
+                        queue.add(cache);
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            for (File file : files) {
+                file.delete();
+            }
+            System.out.println("Merge done");
+        }
+    }
+
+    final class FileCache {
+        private BufferedReader reader;
+        private String cache;
+
+        FileCache(BufferedReader reader) throws IOException {
+            this.reader = reader;
+            this.cache = reader.readLine();
+        }
+
+        String peek() {
+            return this.cache;
+        }
+
+        String poll() throws IOException {
+            String answer = this.cache;
+            this.cache = reader.readLine();
+            return answer;
+        }
+
+        boolean empty() {
+            return this.cache == null;
+        }
+    }
+
 }
+
